f69ec95a6347eecc1680470ef9938a2c321c85f5
user2830581 <102972749+user2830581@users.noreply.github.com>
2024-07-21 17:59:39 +0100

    Update App.js to add more pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,28 @@
 //import css here
 
-import React from 'react';
-import {Routes, Route} from 'react-router-dom';
-import Home from './components/Home';
-import NavBar from './components/NavBar';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./styles/Global.css";
+import Home from "./components/HomePage";
+import Login from "./components/LoginPage";
+import Register from "./components/RegisterPage";
+import NavBar from "./components/NavBar";
+import Dashboard from "./components/DashboardPage";
+import Settings from "./components/SettingsPage";
+import Upload from "./components/UploadPage";
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
-        <Route path="" element={<Home/>}/>
-        <Route path="/navbar" element={<NavBar/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </div>
   );
