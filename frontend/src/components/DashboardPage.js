5ffd10d095d398095deeba2faa90d5f31ebeed91
user2830581 <102972749+user2830581@users.noreply.github.com>
2024-07-21 18:00:19 +0100

    Add React dashboard page

---
+++ frontend/src/components/DashboardPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Home.css';
+
+const DashboardPage = () => {
+  return (
+    <div className="dashboard">
+        <div className="title">
+        <h1>Dashboard</h1>
+        <p>Here's your summary</p>
+        <hr />
+      </div>
+    </div>
+  );
+}
+
+export default DashboardPage;
