72ee6bc64a55f0af1e3651b1cc93285d0c1e679d
user2830581 <102972749+user2830581@users.noreply.github.com>
2024-07-21 18:37:13 +0100

    Rename Navbar.cc to Nav.css, updated import in Nav.js

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/Navbar.css";
+import "../styles/Nav.css";
 import { IoLeaf } from "react-icons/io5";
 import { Link, NavLink } from "react-router-dom";
 
