6d93966fa832c624925c8e57f0aadebdb364c3a4
user2830581 <102972749+user2830581@users.noreply.github.com>
2024-07-21 17:36:09 +0100

    Add React settings page

---
+++ frontend/src/components/SettingsPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//import '../styles/Home.css';
+
+const SettingsPage = () => {
+  return (
+    <div className="settings">
+        <div className="title">
+        <h1>Settings</h1>
+        <p>Manage your account settings and preferences</p>
+        <hr />
+      </div>
+      <div className="settings-section">
+
+      </div>
+      <div className="settings-section">
+
+      </div>
+    </div>
+  );
+}
+
+export default SettingsPage;
