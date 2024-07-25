e0ff5c2dbf70f954e137c934de4369e5a0e0cf73
user2830581 <102972749+user2830581@users.noreply.github.com>
2024-07-25 21:03:27 +0100

    Create 'hooks' folder to manage custom React hooks. Create useModal.js to manage modal visibility

---
+++ frontend/src/hooks/useModal.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+/*
+  Custom React hook to manage login and register modal visbility
+*/
+
+export default function useModal() {
+  // Track which modal is currently active, initally no modal is open
+  const [currentModal, setCurrentModal] = useState(false);
+
+  // Open a specified modal ('register' or 'login')
+  const openModal = (modal) => setCurrentModal(modal);
+
+  // Close the current modal
+  const closeModal = () => setCurrentModal(false);
+
+  return {
+    // Boolean check if current modal is set to 'register' or 'login'
+    isRegisterModalOpen: currentModal === "register",
+    isLoginModalOpen: currentModal === "login",
+    
+    // Function calls to manage modals visibility
+    openRegisterModal: () => openModal("register"),
+    openLoginModal: () => openModal("login"),
+    closeModal,
+  };
+}
