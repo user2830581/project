0a225853bd9b5c04d55fdfc3a88d5269a58b935d
user2830581 <102972749+user2830581@users.noreply.github.com>
2024-07-25 21:20:24 +0100

    Added login and register modal functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,45 @@
-//import css here
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./components/HomePage";
-import Login from "./components/LoginPage";
-import Register from "./components/RegisterPage";
 import Nav from "./components/Nav";
 import Dashboard from "./components/DashboardPage";
 import Settings from "./components/SettingsPage";
 import Upload from "./components/UploadPage";
+import Modal from "./components/Modal";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+import useModal from "./hooks/useModal";
 
+const App = () => {
+  
+  // Initialise useModal hook
+  const {
+    isRegisterModalOpen,
+    isLoginModalOpen,
+    openRegisterModal,
+    openLoginModal,
+    closeModal
+  } = useModal();
 
-function App() {
   return (
     <div className="App">
-      <Nav />
+      <Nav onLoginClick={openLoginModal} />
       <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="" element={<Home onRegisterClick={openRegisterModal} />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/upload" element={<Upload />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
+
+      {/* Log in and Register Modals */}
+      <Modal isOpen={isRegisterModalOpen} onClose={closeModal}>
+        <RegisterForm onLoginClick={openLoginModal} />
+      </Modal>
+
+      <Modal isOpen={isLoginModalOpen} onClose={closeModal}>
+        <LoginForm onSignupClick={openRegisterModal} />
+      </Modal>
     </div>
   );
 }
